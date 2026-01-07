521a1eae97ae2a6fb699043a342cda9d73fea836
System <dimasoft976@gmail.com>
2026-01-07 19:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from kernel.kernel import *
+import uasyncio as asyncio
 
 proc_code1 = '''
+[HEADER]:
+pid:1
+prio:0
+name:filetest
+[ATTRS]
+[PROG]
 print(f"USER: Started with PID {pid}")
 
 request = {
@@ -29,7 +36,7 @@ print(f"USER: Result from FS: {data}")
 '''
 
 if __name__ == "__main__":
-    create_proc(proc_code1, 1)
+    create_proc(proc_code1)
     boot()
 
     asyncio.run(scheduler())
